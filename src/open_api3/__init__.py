ecf46bf1e044d21df3a186464adb5af89560bb10
sven <474416133@qq.com>
2019-07-08 00:51:13 +0800

    init..

---
+++ src/open_api3/__init__.py
@@ -0,0 +1,12 @@
+#-*- encoding:utf-8 -*-
+"""
+Copyrigth : 
+Project Name： openAPI3
+Module Name： __init__.py
+Author : sven
+date： 19-7-8
+Description : 
+
+"""
+__author__ = "sven"
+__email__ = "474416133@qq.com"
---
+++ src/open_api3/objects.py
@@ -0,0 +1,18 @@
+#-*- encoding:utf-8 -*-
+"""
+Copyrigth : 
+Project Name： openAPI3
+Module Name： objects
+Author : sven
+date： 19-7-8
+Description : 
+
+"""
+__author__ = "sven"
+__email__ = "474416133@qq.com"
+
+"""
+open API3 对象
+ref：https://github.com/OAI/OpenAPI-Specification/blob/master/versions/2.0.md#data-types
+
+"""
